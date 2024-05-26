6e96aaa56f4c6d1c2ea46361287e9207a635fee6
Oleg_Karlin <o.karlin1981@gmail.com.>
2024-05-26 18:00:45 +0300

    Add Task3.1

---
+++ Task3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задание 3. Совместная работа
+
+// 1. Считать строку с консоли, состоящую из латинских букв в нижнем регистре. 
+// 2. Выяснить, сколько среди введённых букв гласных.
+
+// Пример:
+// “hello” => 2
+// “world” => 1
+
+// То же самое, но с использованием метода "Contains"
+
+int CountVowelsLetters(string str)
+{
+    string vowels = "aeiouy";
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (vowels.Contains(str[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Напишите слово на латинице");
+string start = Console.ReadLine();
+Console.WriteLine($"В введённом слове {CountVowelsLetters(start)} глассных");
