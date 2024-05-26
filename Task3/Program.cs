88f88c752adf5f022fbe87ea2f4b500f457b213a
Oleg_Karlin <o.karlin1981@gmail.com.>
2024-05-26 17:31:46 +0300

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задание 3. Совместная работа
+
+// 1. Считать строку с консоли, состоящую из латинских букв в нижнем регистре. 
+// 2. Выяснить, сколько среди введённых букв гласных.
+
+// Пример:
+// “hello” => 2
+// “world” => 1
+
+int CountVowelsLetters(string str)
+{
+    string vowels = "aeiouy";
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (vowels[j] == str[i])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Напишите слово на латинице");
+string start = Console.ReadLine();
+Console.WriteLine($"В введённом слове {CountVowelsLetters(start)} глассных");
