b2936e678f5ff05a8654c1560782ad7e72243e05
Oleg_Karlin <o.karlin1981@gmail.com.>
2024-05-26 15:14:37 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задание 1. Совместная работа
+
+// 1. Задайте массив символов (тип char []). 
+// 2. Создайте строку из символов этого массива.
+
+// Указание:
+// Конструктор строки вида string(char []) не использовать.
+
+// char[] chars = new char[] { 'a', 'b', 'c', 'd' };
+// string str = new string(chars);
+
+// Пример:
+// ['a', 'b', 'c', 'd'] => "abcd"
+
+string CharArrayToString(char[] arr)
+{
+    string res = string.Empty; // string.Empty - конструкция для возврата пустой строки (заменяет "")
+    for (int i = 0; i < arr.Length; i++)
+    {
+        res += arr[i];
+    }
+    return res;
+}
+
+char[] array = { 'a', 'b', 'c', 'd', 'e', 'f' };
+string result = CharArrayToString(array);
+Console.WriteLine(result);
