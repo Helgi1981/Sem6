2d5c2c31909f7b2be31c5a4c3fbd77340aa516cc
Oleg_Karlin <o.karlin1981@gmail.com.>
2024-05-28 13:35:53 +0300

    Add Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿// Необходимо разделить логику алгоритмов на функции
+
+// 1. Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// 2. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+// Пример:
+// “aBcD1ef!-” 
+// => “abcd1ef!-”
+
+// Функция для чтения строки из консоли
+string ReadInputString()
+{
+    Console.WriteLine("Напишите слово на латинице");
+    return Console.ReadLine();
+}
+
+// Функция для преобразования всех заглавных букв в строчные
+string ConvertToLowerCase(string input)
+{
+    return input.ToLower();
+}
+
+// Вызов функции для чтения строки
+string start = ReadInputString();
+
+// Вызов функции для преобразования строки
+string result = ConvertToLowerCase(start);
+
+// Вывод результата
+Console.WriteLine(result);
